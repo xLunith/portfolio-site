1c47cc439fcf5edcb8880f1a6f65d5d3eddb8f26
xLunith <xLunith@users.noreply.github.com>
2020-09-15 21:49:37 -0700

    Made animtion finsih by clearing all elements

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import React from 'react'
 import { homedir } from 'os';
@@ -7,6 +8,8 @@ const inputNameArr = ["Clickabl", "Clickab", "Clicka", "Click", "Clic", "Cli", "
 "李維安", "李維", "李", "\n", "autoA Programmer", "\n", "/", "/c", "/cl", "/cle", "/clea", "/clear", "/clear", "/clear"];
 
 const nameAnimArr = [];
+
+const linkArr = [["A", "Ab", "Abo", "Abou", "About"], ["P", "Pr", "Pro", "Proj", "Proje", "Projec", "Project", "Projects"], ["C", "Co", "Con", "Cont", "Conta", "Contac", "Contact"]];
 
 function wordAutomation(){
   inputNameArr.forEach((ele, ind) => {
@@ -27,25 +30,44 @@ function wordAutomation(){
       nameAnimArr.push(ele);
     }
   });
-  console.log(nameAnimArr);
 }
 
 
 class Home extends React.Component {
   constructor(props){
     super(props);
+    //Declaring Refs
     this.name_ref = React.createRef();
     this.welcome_ref = React.createRef();
+    this.about_ref = React.createRef();
+    this.project_ref = React.createRef();
+    this.contact_ref = React.createRef();
+    this.cursor1_ref = React.createRef();
+    this.cursor2_ref = React.createRef();
+    this.cursor3_ref = React.createRef();
+    this.cursor4_ref = React.createRef();
 
     this.nameAnim = () => {
       var speed = 125;
+
+      //Setting Refs
       var name = this.name_ref.current;
       var welcome = this.welcome_ref.current;
+      var about = this.about_ref.current;
+      var project = this.project_ref.current;
+      var contact = this.contact_ref.current;
+      var cursor1 = this.cursor1_ref.current; //Main cursor
+      var cursor2 = this.cursor2_ref.current; //Link cursors
+      var cursor3 = this.cursor3_ref.current;
+      var cursor4 = this.cursor4_ref.current;
+
+      const cursorList = [cursor2, cursor3, cursor4];
+
+      name.style.pointerEvents = 'none';
       nameAnimArr.forEach((ele, ind) => {
           
         var timeoutHandle = setTimeout(function(){
           name.textContent = ele;
-          console.log(ele)
         }, speed*ind);   
         
       });
@@ -53,6 +75,10 @@ class Home extends React.Component {
       setTimeout(function(){
         name.textContent = "";
         welcome.textContent = "";
+        about.textContent = "";
+        project.textContent = "";
+        contact.textContent = "";
+        cursor1.style.display = 'none';
       }, speed*nameAnimArr.length);
       
       
@@ -63,28 +89,41 @@ class Home extends React.Component {
   render(){
     wordAutomation();
     return (
+    <>
+      
       <div className="home-wrapper">
+        <div className="navbar">
+            <div className="name-row"><Link href=""><a ref={this.about_ref} className="montserrat slide-underline">About</a></Link><div className="nav-cursor montserrat blink" ref={this.cursor2_ref}>_</div></div>
+            <div className="name-row"><Link href=""><a ref={this.project_ref} className="montserrat slide-underline">Projects</a></Link><div className="nav-cursor montserrat blink" ref={this.cursor3_ref}>_</div></div>
+            <div className="name-row"><Link href=""><a ref={this.contact_ref} className="montserrat slide-underline">Contact</a></Link><div className="nav-cursor montserrat blink" ref={this.cursor4_ref}>_</div></div>
+        </div>
         <div className="name-wrapper">
+          
           <div className="welcome-text montserrat" ref={this.welcome_ref}>Hi, I'm</div>
           <div className="name-row">
             <div onClick={this.nameAnim} className="welcome-name montserrat" ref={this.name_ref}>Clickable</div>
-            <div className="name-cursor montserrat blink">_</div>
+            <div className="name-cursor montserrat blink" ref={this.cursor1_ref}>_</div>
           </div>
           
         </div>
-        
-        <style jsx>{`
+      </div>
+      <style jsx>{`
+        .navbar{
+          padding: 5vh;
+          display: flex;
+          justify-content: space-evenly;
+        }
+
+
         .home-wrapper{
           width: 100%;
           height: 100%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-  
+          display: contents;
         }
   
         .name-wrapper{
           width: 100%;
+          margin-top: 20vh;
           padding-bottom: 15vh;
           display: grid;
           justify-items: center;
@@ -109,12 +148,16 @@ class Home extends React.Component {
         .name-cursor{
           font-size: 10vh;
           padding-left: 10px;
-          
         }
+
+        .nav-cursor{
+          display: none;
+        }
+
   
       
       `}</style>
-      </div>
+      </>
     )
   }
   
